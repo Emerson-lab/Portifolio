95d2abd3646614aa39dccf2dad12229d8d45d83b
trinddade29@gmail.com <trinddade29@gmail.com>
2021-12-13 23:14:20 -0300

    HomeHero component completion

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,18 +1,42 @@
-import { Container, TextContainer } from './styles';
-import picture from '../../assets/pessoa.webp'
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import picture from '../../assets/pessoa.webp';
 
 function HomeHero() {
   return (
     <Container>
-      <img src={picture} alt="Minha Foto"/>
+      <img src={picture} alt="Minha Foto" />
       <div>
         <TextContainer>
           <h1>Olá</h1>
           <h2>Me chamo Emerson</h2>
         </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className="comment">//Minha apresentação</span>
+            <span className="purple">Infos</span> {'\u007b'}
+            <div>
+              Nome: <span className="blue">Emerson,</span>
+            </div>
+            <div>
+              Sobrenome: <span className="blue">Trindade</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem>
+
+            <span className="purple">Cargo</span> {'\u007b'}
+            <div>
+              Função: <span className="blue">Dev Front-end,</span>
+            </div>
+            <div>
+              Empresa: <span className="blue">Fasters</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
       </div>
     </Container>
   );
-};
+}
 
 export default HomeHero;
