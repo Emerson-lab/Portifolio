7115f21059d2054d2f9cc342ae951b0a7563e343
trinddade29@gmail.com <trinddade29@gmail.com>
2021-12-14 22:49:27 -0300

    Create Experiencias component

---
+++ src/components/Experiencias/ExperienciaItem.tsx
@@ -0,0 +1,13 @@
+import {ItemContainer} from './styles'
+
+export default function ExperienciaItem() {
+  return (
+    <ItemContainer>
+      <div>
+        <h1>2021</h1>
+        <h2>Dev Front-end</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing el</p>
+      </div>
+    </ItemContainer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import {HomeContainer} from '../styles/HomeStyles';
 import Header from '../components/Header';
 import HomeHero from '../components/HomeHero';
+import Experiencias from '../components/Experiencias';
 
 export default function Home() {
   return (
     <HomeContainer>
       <Header />
-
       <main className="container" >
         <HomeHero/>
+        <Experiencias/>
       </main>
     </HomeContainer>
   );
