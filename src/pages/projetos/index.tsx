ae5381694d784a72e429884ea5d777d32a999974
trinddade29@gmail.com <trinddade29@gmail.com>
2021-12-19 14:51:46 -0300

    Create page projetos and css

---
+++ src/pages/projetos/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import { ProjetoContainer } from "../../styles/ProjetosStyles";
+
+export default function Projetos(){
+  return (
+    <ProjetoContainer>
+      <Header/>
+      <main className='container'>
+        <p>oi</p>
+      </main>
+    </ProjetoContainer>
+  )
+}
